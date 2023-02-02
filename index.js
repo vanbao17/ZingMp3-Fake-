029efe671a89c03ef402c7001a1bfd64924affca
vanbao17 <vanpao171103@gmail.com>
2023-02-02 17:32:29 +0700

    ...

--- index.js
+++ index.js
@@ -586,29 +586,29 @@ const personal = {
             processVolume.value = 20
         }
     },
-    handleTimeSong:function(s) {
-        if(s<personal.songs.length)
-        {
-            audio.src = personal.songs[s].path
-            audio.onloadedmetadata = function() {
-                personal.songs.forEach(function(song,index) {
-                    if(s==index)
-                    {
-                        const listsongtime = $$('.list-songs .song .time .listtimesong')
-                        listsongtime[s].textContent = personal.secondsToHms(audio.duration)   
-                    }
-                })
-                personal.handleTimeSong(s+1)
-            }
-        }
-    },
+    // handleTimeSong:function(s) {
+    //     if(s<personal.songs.length)
+    //     {
+    //         audio.src = personal.songs[s].path
+    //         audio.onloadedmetadata = function() {
+    //             personal.songs.forEach(function(song,index) {
+    //                 if(s==index)
+    //                 {
+    //                     const listsongtime = $$('.list-songs .song .time .listtimesong')
+    //                     listsongtime[s].textContent = personal.secondsToHms(audio.duration)   
+    //                 }
+    //             })
+    //             personal.handleTimeSong(s+1)
+    //         }
+    //     }
+    // },
     start:function() {
         this.defineproperties()
         this.rendersong()
         this.rederlibary()
         this.rederplaylist()
         this.handleEvent()
-        this.handleTimeSong(0);
+        // this.handleTimeSong(0);
     }
 }
 personal.start()
