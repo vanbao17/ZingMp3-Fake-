baf1811c50bc9228effd63953741a7023eb91bbc
vanbao17 <vanpao171103@gmail.com>
2023-02-07 16:39:53 +0700

    ...

--- index.js
+++ index.js
@@ -88,27 +88,27 @@ const personal = {
             name:'Định Mệnh',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/9514eaaab0ea59b400fb/4567263235643082012?authen=exp=1675520334~acl=/9514eaaab0ea59b400fb/*~hmac=5a946ce0bdce2cf6093eb7ec17644b34&fs=MTY3NTM0NzUzNDYxNXx3ZWJWNnwwfDQyLjExOS4xMDEdUngNjM',
+            path:'songs/song4.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/6/d/e/b/6deb8ea62749618d200bbe270ade7f3e.jpg'
         },
         {
             name:'Chạm khẽ tim anh một chút thôi',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/789aa00a7b4e9210cb5f/2046322050105663264?authen=exp=1675520575~acl=/789aa00a7b4e9210cb5f/*~hmac=991d1651c00a3c7e1dea77ec5c84adb0&fs=MTY3NTM0NzmUsIC3NTQyNHx3ZWJWNnwwfDEyNS4yMzUdUngMjEyLjgx',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/6/d/e/b/6deb8ea62749618d200bbe270ade7f3e.jpg'
         },
         {
             name:'Em đã thương người ta hơn anh',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/45770b085249bb17e258/5683485288626074057?authen=exp=1675520643~acl=/45770b085249bb17e258/*~hmac=7f469e5d7b63226151cb140ccc30de1b&fs=MTY3NTM0Nzg0MzkyM3x3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/6/d/e/b/6deb8ea62749618d200bbe270ade7f3e.jpg'
         },
         {
             name:'Yêu một người sao buồn đến thế',
             singer:'Noo Phước Thịnh',
-            path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/a73d46c0f88711d94896/6266499990891969805?authen=exp=1675520224~acl=/a73d46c0f88711d94896/*~hmac=17620485ada37b9095f6b6890403f8de&fs=MTY3NTM0NzQyNDI0Mnx3ZWJWNnwwfDExMy4xNzYdUngNjIdUngMjI3',
+            path:'songs/song6.mp3',
             album:'',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/e/0/c/3/e0c341956a038d66b77275b20168b112.jpg'
         },
@@ -116,114 +116,121 @@ const personal = {
             name:'Quay về đi',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-16-tf-mp3-320s1-zmp3.zmdcdn.me/675ac4b774f39dadc4e2/1086650921171799167?authen=exp=1675521613~acl=/675ac4b774f39dadc4e2/*~hmac=2cbd2a0b8a0565258177102472f40319&fs=MTY3NTM0ODgxMzEyNHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/avatars/6/5/1/6/6516b96ae8353ef11f6b7148c74bb387.jpg'
         },
         {
             name:'Cause I Love You',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/5eed38d295957ccb2584/5770294300014409713?authen=exp=1675521646~acl=/5eed38d295957ccb2584/*~hmac=f4c84e892493438cf56c955f5ceb2373&fs=MTY3NTM0ODg0NjU0M3x3ZWJWNnwwfDE0LjI0OS4xNDYdUngMjA2',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/8/f/0/d/8f0da549f6cf94288361aac93d05d284.jpg'
         },
         {
             name:'Thương mấy cũng là người dưng',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/1e93a0d8d3993ac76388/9192501519108808551?authen=exp=1675521456~acl=/1e93a0d8d3993ac76388/*~hmac=105ee740da166459d26021b1b8df2cf3&fs=MTY3NTM0ODY1NjUxOHx3ZWJWNnwxMDE0NzU4NDY3fDExNS43Mi4yMTmUsICdUngODU',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/8/f/0/d/8f0da549f6cf94288361aac93d05d284.jpg'
         },
         {
             name:'Cơn mơ băng giá',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-24-tf-mp3-320s1-zmp3.zmdcdn.me/712b72635625bf7be634/6150793611726653214?authen=exp=1675521689~acl=/712b72635625bf7be634/*~hmac=c479d721b88a3f24d9e86ed9a6698a80&fs=MTY3NTM0ODg4OTmUsICwM3x3ZWJWNnwwfDE3MS4yMjUdUngMjUwLjEwMg',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/b/d/d/9/bdd90e2f31c41756ba054b6d96ccd674.jpg'
         },
         {
             name:'Những kẻ mộng mơ',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-24-tf-mp3-320s1-zmp3.zmdcdn.me/712b72635625bf7be634/6150793611726653214?authen=exp=1675521757~acl=/712b72635625bf7be634/*~hmac=deacd3ca668f65ee70fd05feab4172dc&fs=MTY3NTM0ODk1NzM5Mnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/1/4/3/c/143c3e0f7a42b90009f5738899280003.jpg'
         },
         {
             name:'Chợt thấy em khóc',
             singer:'Noo Phước Thịnh',
             album:'',
-            path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/f404425394147d4a2405/8914220867562786807?authen=exp=1675521824~acl=/f404425394147d4a2405/*~hmac=3bf0f9df529175f60bcef7cfaad2404a&fs=MTY3NTM0OTAyNDI3NXx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+            path:'songs/song6.mp3',
             image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/9/7/c/0/97c0d89266b572d570062a0b7abbb99a.jpg'
         },
-        {
-            name:'Đi đâu để thấy hoa bay',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/df7989e5dfa436fa6fb5/554259536577642684?authen=exp=1675521843~acl=/df7989e5dfa436fa6fb5/*~hmac=5ca9432a82a99fecf64f44048d688e53&fs=MTY3NTM0OTA0MzYyNnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/736x/80/1e/61/801e61077fd861bd67d248116c04ad97.jpg'
-        },
-        {
-            name:'Chưa Bao Giờ',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/f7e2a163f7221e7c4733/4703424405522837128?authen=exp=1675521866~acl=/f7e2a163f7221e7c4733/*~hmac=8398919c852dc2f9516699faf7949cd6&fs=MTY3NTM0OTA2NjQ1NHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/564x/e3/9c/9e/e39c9ea172a31c46fc9289212b593680.jpg'
-        },
-        {
-            name:'Giâc mơ chỉ là giấc mơ',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-23-tf-mp3-320s1-zmp3.zmdcdn.me/76d47248ab0c42521b1d/3318461162738757549?authen=exp=1675521891~acl=/76d47248ab0c42521b1d/*~hmac=43d5d0193626701420c0d464c082ccaf&fs=MTY3NTM0OTA5MTkyNXx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/564x/bf/b4/b7/bfb4b7d496fb3046d7f07ee6e22230ac.jpg'
-        },
-        {
-            name:'Mashup chỉ còn những mùa nhớ',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/6cd6284a7e0b9755ce1a/2178783514501594317?authen=exp=1675521918~acl=/6cd6284a7e0b9755ce1a/*~hmac=c2164e76b2763d1afa9acabcbe7f88c4&fs=MTY3NTM0OTExODM3Nnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/564x/88/d6/f6/88d6f633996932935d43dca34c305820.jpg'
-        },
-        {
-            name:'Người',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/9e557ca7e5e30cbd55f2/1932857558382396677?authen=exp=1675521948~acl=/9e557ca7e5e30cbd55f2/*~hmac=71f4b8948854b66886524b0657ca4bc5&fs=MTY3NTM0OTE0ODMzMXx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/736x/2e/d1/54/2ed15444732711e74a184a8d5da401cd.jpg'
-        },
-        {
-            name:'Người tình mùa đông',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-16-tf-mp3-320s1-zmp3.zmdcdn.me/3b5a79db2f9ac6c49f8b/2878669073906586910?authen=exp=1675521969~acl=/3b5a79db2f9ac6c49f8b/*~hmac=4443615059e617035ed9bf7b9ef76fe5&fs=MTY3NTM0OTE2OTI4OHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/736x/bc/58/9b/bc589b90ac406a936ec647eb8df34eeb.jpg'
-        },
-        {
-            name:'Phố mùa đông',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/3fe11d604b21a27ffb30/4669118512350951093?authen=exp=1675521987~acl=/3fe11d604b21a27ffb30/*~hmac=c41b04fe95b137d7ceb6adf1a241808a&fs=MTY3NTM0OTE4NzMwMnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/564x/0b/34/d4/0b34d4444fea16aac0685d2a115ab511.jpg'
-        },
-        {
-            name:'Tháng mấy em nhớ anh ?',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-16-tf-mp3-320s1-zmp3.zmdcdn.me/6b28d4118d50640e3d41/5181445787208545549?authen=exp=1675521506~acl=/6b28d4118d50640e3d41/*~hmac=5eb6b366af214f01692c6ac912c22022&fs=MTY3NTM0ODmUsICwNjMzNXx3ZWJWNnwxMDA4ODYwMjE1fDEdUngNTMdUngMTQ0Ljkw',
-            image:'https://i.pinimg.com/736x/7f/b2/a9/7fb2a9e07ffc6b4546e95e9fa05b020b.jpg'
-        },
-        {
-            name:'Tháng tư là lời nói dối của em',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/ff75ee453401dd5f8410/1589336670738807709?authen=exp=1675521973~acl=/ff75ee453401dd5f8410/*~hmac=44543f80345d92e2fad3cfe374d68724&fs=MTY3NTM0OTE3MzE0Nnx3ZWJWNnwxMDmUsIC1MDYxNzmUsIC5fDExMy4xOTEdUngMTgyLjYw3',
-            image:'https://i.pinimg.com/736x/86/73/f0/8673f00fed92c363da30de4492b241a3.jpg'
-        },
-        {
-            name:'Trái tim em cũng biết đau',
-            singer:'Hà Anh Tuấn',
-            album:'',
-            path:'https://vnso-zn-23-tf-mp3-320s1-zmp3.zmdcdn.me/89d6ad57fb1612484b07/1897209746444752983?authen=exp=1675522051~acl=/89d6ad57fb1612484b07/*~hmac=02abdb02e3256ae6b18379509695c9b8&fs=MTY3NTM0OTI1MTY3OHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
-            image:'https://i.pinimg.com/736x/21/5c/ce/215ccebf41f0805969ca33c09daf7722.jpg'
-        }
+        // {
+        //     name:'Đi đâu để thấy hoa bay',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/df7989e5dfa436fa6fb5/554259536577642684?authen=exp=1675521843~acl=/df7989e5dfa436fa6fb5/*~hmac=5ca9432a82a99fecf64f44048d688e53&fs=MTY3NTM0OTA0MzYyNnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/736x/80/1e/61/801e61077fd861bd67d248116c04ad97.jpg'
+        // },
+        // {
+        //     name:'Chưa Bao Giờ',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/f7e2a163f7221e7c4733/4703424405522837128?authen=exp=1675521866~acl=/f7e2a163f7221e7c4733/*~hmac=8398919c852dc2f9516699faf7949cd6&fs=MTY3NTM0OTA2NjQ1NHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/564x/e3/9c/9e/e39c9ea172a31c46fc9289212b593680.jpg'
+        // },
+        // {
+        //     name:'Giâc mơ chỉ là giấc mơ',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-23-tf-mp3-320s1-zmp3.zmdcdn.me/76d47248ab0c42521b1d/3318461162738757549?authen=exp=1675521891~acl=/76d47248ab0c42521b1d/*~hmac=43d5d0193626701420c0d464c082ccaf&fs=MTY3NTM0OTA5MTkyNXx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/564x/bf/b4/b7/bfb4b7d496fb3046d7f07ee6e22230ac.jpg'
+        // },
+        // {
+        //     name:'Mashup chỉ còn những mùa nhớ',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-10-tf-mp3-320s1-zmp3.zmdcdn.me/6cd6284a7e0b9755ce1a/2178783514501594317?authen=exp=1675521918~acl=/6cd6284a7e0b9755ce1a/*~hmac=c2164e76b2763d1afa9acabcbe7f88c4&fs=MTY3NTM0OTExODM3Nnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/564x/88/d6/f6/88d6f633996932935d43dca34c305820.jpg'
+        // },
+        // {
+        //     name:'Người',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/9e557ca7e5e30cbd55f2/1932857558382396677?authen=exp=1675521948~acl=/9e557ca7e5e30cbd55f2/*~hmac=71f4b8948854b66886524b0657ca4bc5&fs=MTY3NTM0OTE0ODMzMXx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/736x/2e/d1/54/2ed15444732711e74a184a8d5da401cd.jpg'
+        // },
+        // {
+        //     name:'Người tình mùa đông',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-16-tf-mp3-320s1-zmp3.zmdcdn.me/3b5a79db2f9ac6c49f8b/2878669073906586910?authen=exp=1675521969~acl=/3b5a79db2f9ac6c49f8b/*~hmac=4443615059e617035ed9bf7b9ef76fe5&fs=MTY3NTM0OTE2OTI4OHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/736x/bc/58/9b/bc589b90ac406a936ec647eb8df34eeb.jpg'
+        // },
+        // {
+        //     name:'Phố mùa đông',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-15-tf-mp3-320s1-zmp3.zmdcdn.me/3fe11d604b21a27ffb30/4669118512350951093?authen=exp=1675521987~acl=/3fe11d604b21a27ffb30/*~hmac=c41b04fe95b137d7ceb6adf1a241808a&fs=MTY3NTM0OTE4NzMwMnx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/564x/0b/34/d4/0b34d4444fea16aac0685d2a115ab511.jpg'
+        // },
+        // {
+        //     name:'Tháng mấy em nhớ anh ?',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-16-tf-mp3-320s1-zmp3.zmdcdn.me/6b28d4118d50640e3d41/5181445787208545549?authen=exp=1675521506~acl=/6b28d4118d50640e3d41/*~hmac=5eb6b366af214f01692c6ac912c22022&fs=MTY3NTM0ODmUsICwNjMzNXx3ZWJWNnwxMDA4ODYwMjE1fDEdUngNTMdUngMTQ0Ljkw',
+        //     image:'https://i.pinimg.com/736x/7f/b2/a9/7fb2a9e07ffc6b4546e95e9fa05b020b.jpg'
+        // },
+        // {
+        //     name:'Tháng tư là lời nói dối của em',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-5-tf-mp3-320s1-zmp3.zmdcdn.me/ff75ee453401dd5f8410/1589336670738807709?authen=exp=1675521973~acl=/ff75ee453401dd5f8410/*~hmac=44543f80345d92e2fad3cfe374d68724&fs=MTY3NTM0OTE3MzE0Nnx3ZWJWNnwxMDmUsIC1MDYxNzmUsIC5fDExMy4xOTEdUngMTgyLjYw3',
+        //     image:'https://i.pinimg.com/736x/86/73/f0/8673f00fed92c363da30de4492b241a3.jpg'
+        // },
+        // {
+        //     name:'Trái tim em cũng biết đau',
+        //     singer:'Hà Anh Tuấn',
+        //     album:'',
+        //     path:'https://vnso-zn-23-tf-mp3-320s1-zmp3.zmdcdn.me/89d6ad57fb1612484b07/1897209746444752983?authen=exp=1675522051~acl=/89d6ad57fb1612484b07/*~hmac=02abdb02e3256ae6b18379509695c9b8&fs=MTY3NTM0OTI1MTY3OHx3ZWJWNnwxMDQ4NDI0NzmUsICxfDU5LjE1My4yMjQdUngMjI',
+        //     image:'https://i.pinimg.com/736x/21/5c/ce/215ccebf41f0805969ca33c09daf7722.jpg'
+        // },
+        // {
+        //     name:'Lối nhỏ',
+        //     singer:'Đen',
+        //     album:'',
+        //     path:'https://vnso-zn-23-tf-mp3-s1-zmp3.zmdcdn.me/edc75a7a3f3dd6638f2c/7555328701165411356?authen=exp=1675773773~acl=/edc75a7a3f3dd6638f2c/*~hmac=fa5e8994b6670aaa75695f9a146d3670&fs=MTY3NTYwMDk3MzI0Nnx3ZWJWNnwwfDExOC42OC42LjE3Mw',
+        //     image:'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/2/d/0/b/2d0bf08b1ddaa07bae28208796316571.jpg'
+        // }
     ],
     defineproperties: function() {
         Object.defineProperty(this,'currentSong',{
@@ -596,10 +603,7 @@ const personal = {
         this.rederlibary()
         this.rederplaylist()
         this.handleEvent()
-        // setTimeout(function() {
-        //     personal.handleTimeSong(0);
-        // },1000)
-            personal.handleTimeSong(0);
+        personal.handleTimeSong(0);
     }
 }
 personal.start()
